56b382e37dd5076f496c9d8ea97cac24746f5d6d
ArtyomYam <114959219+ArtyomYam@users.noreply.github.com>
2022-10-11 00:45:04 +0300

    Add files via upload

---
+++ codefoces 263a/codefoces 263a/codefoces 263a.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    int y, x, numb;
+    for (int i = 0; i < 5; i += 1) {
+        for (int j = 0; j < 5; j += 1) {
+            std::cin >> numb;
+            if (numb == 1) {
+                y = i;
+                x = j;
+            }
+        }
+    }
+    std::cout << y << ' ' << abs(3 - x) << ' ' << abs(3 - y);
+    std::cout << abs(3 - x) + abs(3 - y);
+}
---
+++ codeforces 282a/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	int t, x;
+	char miss, get, miss2;
+	x = 0;
+	std::cin >> t;
+	for (int q = 0; q < t; q += 1) {
+		std::cin >> miss >> get >> miss2;
+		if (get == '+') {
+			x += 1;
+		}
+		if (get == '-') {
+			x -= 1;
+		}
+	}
+	std::cout << x;
+}
