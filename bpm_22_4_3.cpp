73a82264cc49f9466852b4b0067eb0fb3d912729
ArtyomYam <114959219+ArtyomYam@users.noreply.github.com>
2022-12-20 12:11:05 +0300

    Add files via upload

---
+++ bpm_22_4_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int count = 0;
+    std::string first, second;
+    std::cin >> first >> second;
+    int fsize, ssize, n;
+    fsize = first.size();
+    ssize = second.size();
+    if (fsize > ssize) {
+        n = fsize;
+    }
+    else {
+        n = ssize;
+    }
+    if (fsize - ssize < 0 or fsize - fsize > 0) {
+        if (fsize - ssize < 0) {
+            first += '0' * (ssize - fsize);
+        }
+        else {
+            second += '0' * (fsize - ssize);
+        }
+    }
+    while (first[n] == second[n]){
+        count += 1;
+        n -= 1;
+    }
+    std::cout << count;
+}
