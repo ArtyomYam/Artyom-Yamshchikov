d0831df8cf5135768a9518936e2ae68c2c189d11
ArtyomYam <114959219+ArtyomYam@users.noreply.github.com>
2022-12-27 12:20:42 +0300

    Add files via upload

---
+++ codeforces 1670B.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	int t, n, k;
+	std::vector<int>numbers(250);
+	unsigned char ch = 0;
+	std::cin >> t;
+	for (int q = 0; q < t; q += 1) {
+		std::cin >> n;
+		numbers[n] = 1;
+
+	}
+}
