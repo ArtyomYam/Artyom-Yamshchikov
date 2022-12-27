3587a03b2602f41e41b86460e15074f560a63af6
ArtyomYam <114959219+ArtyomYam@users.noreply.github.com>
2022-12-27 04:12:58 +0300

    Add files via upload

--- bpm_22_4_3.cpp
+++ bpm_22_4_3.cpp
@@ -1,31 +1,46 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <string>
 
 int main()
 {
     int count = 0;
     std::string first, second;
     std::cin >> first >> second;
-    int fsize, ssize, n;
+    unsigned long long fsize, ssize;
+    int n;
     fsize = first.size();
     ssize = second.size();
-    if (fsize > ssize) {
-        n = fsize;
-    }
-    else {
-        n = ssize;
-    }
-    if (fsize - ssize < 0 or fsize - fsize > 0) {
+ //   if (fsize > ssize) {
+ //       n = ssize;
+//    }
+ //   else {
+ //       n = fsize;
+ //   }
+
+ /*  if (fsize - ssize < 0 or fsize - fsize > 0) {
         if (fsize - ssize < 0) {
             first += '0' * (ssize - fsize);
         }
         else {
             second += '0' * (fsize - ssize);
         }
+    }*/
+ //   std::cout << first << '\n' << second;
+ //   int k = n;
+//    while (first[fsize-k] == second[ssize-k] and k <= n)
+ //   while(n) {
+//        count += 1;
+//        k -= 1;
+ //       if (first[fsize - 1] == second[ssize - 1]) {
+//            count += 1;
+    while ((fsize - 1 > -1) && (ssize - 1 > -1) && (first[i - 1] == second[j - 1])) {
+            fsize -= 1;
+            ssize -= 1;
+//        }
+//        else {
+//            break;
+//        }
     }
-    while (first[n] == second[n]){
-        count += 1;
-        n -= 1;
-    }
-    std::cout << count;
+    std::cout << fsize + ssize;
 }
